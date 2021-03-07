adc3d5d939bf7de6be60610ed56dbba943778789
Dominik Kozaczko <dominik@kozaczko.info>
2021-03-07 14:29:41 +0100

    Newest Python

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ fi # [ -f requirements.txt ]
 
 
 # if [[ "$@" == "" ]]; then
-pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
+pyinstaller --clean -y -F --dist ./dist/windows --workpath /tmp $SPEC_FILE
 chown -R --reference=. ./dist/windows
 # else
     # sh -c "$@"
